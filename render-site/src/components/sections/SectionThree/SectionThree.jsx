8cc89fd346cc80ee75fe77ae2e4202b78a4837ab
adeka stanley <adekastanley2@gmail.com>
2023-10-19 15:21:53 +0100

    safty commit, about to route

--- render-site/src/components/sections/SectionThree/SectionThree.jsx
+++ render-site/src/components/sections/SectionThree/SectionThree.jsx
@@ -2,8 +2,15 @@ import "./sectionthree.css";
 
 const SectionThree = () => {
 	return (
-		<div className="sectionTwoWrapper">
+		<div className="sectionThreeWrapper">
 			<h2 id="contact">Get in touch</h2>
+			<p>
+				We're here to answer your questions, provide assistance, and listen to
+				your feedback. Don't hesitate to contact us:
+				<a className="send-email" href="#">
+					send email
+				</a>
+			</p>
 		</div>
 	);
 };
